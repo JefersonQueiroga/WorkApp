18b4661b41bab10951497128315828d5a64a9542
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2025-09-30 08:45:10 -0300

    alteração das migrations

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,44 +4,28 @@ from .models import UsuarioAdaptado
 
 
 class UsuarioAdaptadoCreationForm(UserCreationForm):
-    cpf = forms.CharField(
-        max_length=11,
-        required=True,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Digite seu CPF (apenas números)'
-        })
-    )
-    
     class Meta:
         model = UsuarioAdaptado
-        fields = ['username', 'email', 'cpf', 'nome_cidade', 'nome_mae', 
-                  'endereco', 'nome_bairro', 'password1', 'password2']
+        fields = [
+            'username', 'email', 'cpf', 'nome_cidade', 'nome_mae', 'endereco', 'nome_bairro', 'password1', 'password2'
+        ]
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome de usuário'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'seu@email.com'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite seu CPF (apenas números)'}),
             'nome_cidade': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cidade'}),
             'nome_mae': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome da mãe'}),
             'endereco': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Endereço completo'}),
             'nome_bairro': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Bairro'}),
         }
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Senha'})
         self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Confirme a senha'})
 
 
+
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Nome de usuário'
-        })
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Senha'
-        })
-    )
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Nome de usuário' }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': 'Senha'}))
